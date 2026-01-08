99f810c07a78447fb84c3e3a5c581d3abb256bad
mvmarincek <mvmarincek@users.noreply.github.com>
2026-01-08 13:23:43 -0300

    debug: add eruda mobile console for debugging

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -40,6 +40,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             />
           </>
         )}
+        <script src="https://cdn.jsdelivr.net/npm/eruda"></script>
+        <script dangerouslySetInnerHTML={{ __html: 'eruda.init();' }} />
       </head>
       <body className={inter.className}>
         <AuthProvider>
