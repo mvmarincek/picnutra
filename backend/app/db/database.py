bf342af1b744a04bd26d0ccddc00cf036e474a7c
mvmarincek <mvmarincek@users.noreply.github.com>
2026-01-20 00:19:46 -0300

    fix: adicionar async_session para compatibilidade

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -37,6 +37,9 @@ def get_session_maker():
         )
     return _async_session
 
+def async_session():
+    return get_session_maker()()
+
 async def get_db():
     session_maker = get_session_maker()
     async with session_maker() as session:
